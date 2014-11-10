22f11cb3ecf1948fc8633f573a56352bbd6fa463
DanieleO <daniele.olivotti@fao.org>
2014-11-10 11:49:36 +0100

    Some cleanup

--- js/start.js
+++ js/start.js
@@ -9,6 +9,14 @@ define([
     function DSDEditor_starter() {
         DSDEditorWr = new DSDEditorWr();
         DSDEditorWr.render($('#mainContainer'), null);
+
+        /*var colsAdapter = {
+            //source: "http://faostat3.fao.org:7799/v2/msd/resources/metadata/dan2/1.0?dsd=true",
+            //data: null
+            serviceAddress: "http://faostat3.fao.org:7799/v2/msd/resources/metadata/dan2/1.0?dsd=true",
+            data: null}
+
+        DSDEditorWr.load(colsAdapter);*/
     }
 
     /*Multilang test*/
