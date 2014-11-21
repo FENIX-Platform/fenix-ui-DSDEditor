b0b1db0da48e6c9c0b72c2a992dde1fa663f5112
DanieleO-Fenix <daniele.olivotti@fao.org>
2014-11-21 09:02:33 +0100

    New D3SConnector

--- js/start.js
+++ js/start.js
@@ -1,33 +1,17 @@
 define([
     'jquery',
-//    'fx-DSDEditor/js/DSDEditor/DSDEditorWr',
-'fx-DSDEditor/js/DSDEditor/DSDEditor',
-    'fx-DSDEditor/js/DSDEditor/dataConnectors/D3S_Connector',
+    'fx-DSDEditor/js/DSDEditor/DSDEditor',
+    'fx-DSDEditor/js/DSDEditor/dataConnectors/Connector_D3S',
     'bootstrap',
     'domReady!'
-
-//], function ($, DSDEditorWr, Connector) {
-//], function ($, DSDEditor, DSDEditorBridge, Connector) {
 ], function ($, DSDEditor, Connector) {
 
     function DSDEditor_starter(config, callB) {
         DSDEditor = new DSDEditor(config);
         DSDEditor.render($('#mainContainer'), null, callB);
 
-
-        /*bridge = new DSDEditorBridge();
-        bridge.getDSD("http://faostat3.fao.org/d3s2/v2/msd/resources/metadata", "dan3", null, function (data) { console.log(data); });*/
-
         $('#btnEN').click(function () { setLang('EN'); });
         $('#btnFR').click(function () { setLang('FR'); });
-
-        /*var colsAdapter = {
-         //source: "http://faostat3.fao.org:7799/v2/msd/resources/metadata/dan2/1.0?dsd=true",
-         //data: null
-         serviceAddress: "http://faostat3.fao.org:7799/v2/msd/resources/metadata/dan2/1.0?dsd=true",
-         data: null}
-
-         DSDEditorWr.load(colsAdapter);*/
     }
 
     /*Multilang test*/
@@ -42,12 +26,15 @@ define([
     /*End multilang test*/
 
     function updateDSD(uid, version, dsd, datasource, contextSys, callB) {
-         var conn = new Connector();
-         conn.getMetadata(uid, version, function (meta) {
-             if (!meta)
-                 throw new Error("Cannot find metadata with UID " + uid + " and version " + version);
-             conn.updateDSD(meta, dsd, datasource, contextSys, callB);
-         });
+        /*var conn = new Connector();
+        conn.getMetadata(uid, version, function (meta) {
+            if (!meta)
+                throw new Error("Cannot find metadata with UID " + uid + " and version " + version);
+            conn.updateDSD(meta, dsd, datasource, contextSys, callB);
+        });*/
+
+        var conn = new Connector();
+        conn.updateDSD(uid, version, dsd, datasource, contextSys, callB);
     }
 
     function setColumns(cols) {
